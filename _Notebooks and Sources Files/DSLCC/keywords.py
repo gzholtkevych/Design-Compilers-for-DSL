66be1ac77c5d42416f83e8545126f35d11a0ad05
Grygoriy Zholtkevych <g.zholtkevych@gmail.com>
2024-07-16 17:54:02 +0300

    update

---
+++ _Notebooks and Sources Files/DSLCC/keywords.py
@@ -0,0 +1,8 @@
+def split_text(text: str, whitespaces: str, symbols: str) -> list[str]:
+    wait_char = 0
+    current_state, out_List = wait_char, []
+    for ch in text:
+        
+    pass
+
+# def recognize_keywords(text: str, keywords: list[str])
